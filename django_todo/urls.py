5db7408d2cec6104da3507fd481bf9131f06569c
tarahwaters <thbwaters@outlook.com>
2024-01-29 03:59:36 +0000

    Update URLs import for get_todo_list

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todo.views import say_hello
+from todo.views import get_todo_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
