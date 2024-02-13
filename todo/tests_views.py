eb4c814a65e20253ee01cbca0543e85ae4ca9d4f
tarahwaters <thbwaters@outlook.com>
2024-02-13 19:13:57 +0000

    Add test for get todo_list page

--- todo/tests_views.py
+++ todo/tests_views.py
@@ -2,7 +2,19 @@ from django.test import TestCase
 
 # Create your tests here.
 
-class TestDjango(TestCase):
+class TestViews(TestCase):
 
-    def test_this_thing_works(self):
-        self.assertEqual(1, 0)
+    def test_get_todo_list(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/todo_list.html')
+    
+    # def test_get_add_item_page(self):
+
+    # def test_get_edit_item_page(self):
+    
+    # def test_can_add_item(self):
+    
+    # def test_can_delete_item(self):
+    
+    # def test_can_toggle_item(self):
