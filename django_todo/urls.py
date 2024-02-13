4837050b9ea704abc209b0477f448c6e2c1b381d
tarahwaters <thbwaters@outlook.com>
2024-02-13 17:24:32 +0000

    Fix error in todo views import on line18

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todo.views import views
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
